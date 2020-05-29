53678dae914f20641892f196417c5ab2d37bb464
Nelson Gallardo <gnelson85@gmail.com>
2020-05-29 17:25:59 +0100

    adding scripts for uploading and fixing typo in description

---
+++ upload_to_test_pypi.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Remove dist folder as it will be generated again in the next step with a new build.
+if [ ! -d "dist" ] 
+then
+    echo "Deleting dist directory" 
+    rm -r dist/*
+fi
+
+# Install/update needed tools
+python3 -m pip install --user --upgrade twine setuptools wheel
+
+# Create new build
+python3 setup.py sdist
+
+# Upload new build to pypi using twine
+python3 -m twine upload --repository testpypi dist/*
