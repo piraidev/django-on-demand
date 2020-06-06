85650285749dc1e44294b4f36f4b9aa23e168ec8
Cesar Augusto Garcia <cesaraugustogarcia86@gmail.com>
2020-06-06 14:55:09 -0500

    Update release config setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(install_requires=[
-    'djangorestframework>=3.9.1',
-    'django-filter>=2.0.0'
+    'djangorestframework',
+    'django-filter'
 
 ])
--- upload_to_pypi.sh
+++ upload_to_pypi.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # Remove dist folder as it will be generated again in the next step with a new build.
-if [ ! -d "dist" ] 
+if [ -d "dist" ]
 then
     echo "Deleting dist directory" 
-    rm -r dist/*
+    rm -r dist
 fi
 
 # Install/update needed tools
-python3 -m pip install --user --upgrade twine setuptools wheel
+python3 -m pip install --upgrade twine setuptools wheel
 
 # Create new build
 python3 setup.py sdist
